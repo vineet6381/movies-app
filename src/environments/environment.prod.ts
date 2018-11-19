40fb55c81c46866fd14a37c6ff4a250ac17e1105
Romain Briand <contact@romain-briand.fr>
2018-11-19 18:16:49 +0100

    server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.themoviedb./3",
+  apiUrl: "https://api.themoviedb.org/3",
   apiKey: "f3a7347fd19c436dc3e9858efb1769c5"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://api.themoviedb./3",
+  apiUrl: "https://api.themoviedb.org/3",
   apiKey: "f3a7347fd19c436dc3e9858efb1769c5"
 };
 
